79414222d1a697e89524a8354a88b6514a3cbac3
ubuntu <harikadasari@truequations.com>
2023-07-27 19:35:55 +0530

    apis

--- db/migrate/20230704063908_create_apis.rb
+++ db/migrate/20230704063908_create_apis.rb
@@ -1,20 +1,4 @@
 class CreateApis < ActiveRecord::Migration[6.0]
-#   def change
-#     create_table :apis do |t|
-#       t.string :name
-#       t.string :method
-#       t.string :path
-#       t.string :requestheader
-#       t.string :requestbody
-#       t.references :collection, null: false, foreign_key: true
-
-#       t.timestamps
-#     end
-#     add_index :apis, :collection_id
-
-#   end
-# end
-
 
     create_table :apis do |t|
       t.string :name
--- db/migrate/20230727104300_add_request_params_to_apis.rb
+++
@@ -1,5 +0,0 @@
-class AddRequestParamsToApis < ActiveRecord::Migration[6.0]
-  def change
-    add_column :apis, :request_params, :json, default: {}
-  end
-end
